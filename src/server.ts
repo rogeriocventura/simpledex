03ca33fe6e23756f87e305bc8b509658d030f878
rogeriocventura <rogeriocventura@hotmail.com>
2021-04-12 21:06:37 -0300

    commit apenas npm + server.ts

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+//import { router } from "./config/routes";
+//import { mongoose } from "./config/database";
+
+const app = express();
+//const db = mongoose;
+
+
+console.clear();
+
+//Funcionalidades
+//Métodos/Verbos HTTP -> POST e GET ...
+app.use(express.json())
+//app.use(router);
+
+
+app.listen(3000, () => {
+  console.log("O servidor está rodando...");
+});
